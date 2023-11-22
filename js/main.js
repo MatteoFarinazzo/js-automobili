4a201bb5aae99f7a3bfd6c044e55b0b218509eae
MatteoFarinazzo <matteofary@gmail.com>
2023-11-22 13:54:58 +0100

    array done

---
+++ js/main.js
@@ -0,0 +1,56 @@
+
+let cars = [
+    {
+        marca: "Fiat",
+        modello: "Uno",
+        alimentazione: "Benzina",
+    },
+    {
+        marca: "Renault",
+        modello: "Twingo",
+        alimentazione: "Elettrica",
+    },
+    {
+        marca: "Citroen",
+        modello: "C1",
+        alimentazione: "Ibrida",
+    },
+    {
+        marca: "Opel",
+        modello: "Corsa",
+        alimentazione: "Diesel",
+    },
+    {
+        marca: "Alfa Romeo",
+        modello: "Giulietta",
+        alimentazione: "Benzina",
+    },
+    {
+        marca: "Fiat",
+        modello: "Panda",
+        alimentazione: "Gpl",
+    },
+    {
+        marca: "Volkswagen",
+        modello: "Taigo",
+        alimentazione: "Benzina",
+    },
+    {
+        marca: "Opel",
+        modello: "Mokka",
+        alimentazione: "Ibrida",
+    },
+    {
+        marca: "Smart",
+        modello: "ForFour",
+        alimentazione: "Elettrica",
+    },
+    {
+        marca: "Fiat",
+        modello: "500",
+        alimentazione: "Diesel",
+    },
+
+];
+
+
